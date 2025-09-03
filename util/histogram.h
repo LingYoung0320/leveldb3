2580014efc16161ede9d17da37c72de5bad90681
Ling Young <114047332+LingYoung0320@users.noreply.github.com>
2025-09-03 14:25:42 +0800

    Update histogram.h

--- util/histogram.h
+++ util/histogram.h
@@ -1,3 +1,15 @@
+// 一些无关的测试代码，仅用于验证工具逻辑
+int dummy_function_1() { return 1; }
+int dummy_function_2() { return 2; }
+int dummy_function_3() { return 3; }
+int dummy_function_4() { return 4; }
+int dummy_function_5() { return 5; }
+int dummy_function_6() { return 6; }
+int dummy_function_7() { return 7; }
+int dummy_function_8() { return 8; }
+int dummy_function_9() { return 9; }
+int dummy_function_10() { return 10; }
+
 // Copyright (c) 2011 The LevelDB Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file. See the AUTHORS file for names of contributors.
@@ -11,8 +23,8 @@ namespace leveldb {
 
 class Histogram {
  public:
-  Histogram() { }
-  ~Histogram() { }
+  Histogram() {}
+  ~Histogram() {}
 
   void Clear();
   void Add(double value);
@@ -21,20 +33,22 @@ class Histogram {
   std::string ToString() const;
 
  private:
+  enum { kNumBuckets = 154 };
+
+  double Median() const;
+  double Percentile(double p) const;
+  double Average() const;
+  double StandardDeviation() const;
+
+  static const double kBucketLimit[kNumBuckets];
+
   double min_;
   double max_;
   double num_;
   double sum_;
   double sum_squares_;
 
-  enum { kNumBuckets = 154 };
-  static const double kBucketLimit[kNumBuckets];
   double buckets_[kNumBuckets];
-
-  double Median() const;
-  double Percentile(double p) const;
-  double Average() const;
-  double StandardDeviation() const;
 };
 
 }  // namespace leveldb
